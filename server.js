e0bfc0a9cff712b62db154ad17b7f6777948da03
Gabriel Luis <161089616+gabrielluis12345@users.noreply.github.com>
2025-12-08 17:45:08 -0300

    token atualizado

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json()); // Substitui bodyParser
 app.use(express.static("."));
 
 // CONFIG SUA
-const ACCESS_TOKEN = "APP_USR-4470259940372106-120813-e8e508187572339bf7d5027a95dfcd70-3049555673";
+const ACCESS_TOKEN = "APP_USR-7919662293588734-120813-aad15c42e877020e9ff8a910e1f4173b-749376790";
 const SHEETS_URL = "https://script.google.com/macros/s/AKfycbzSZI_jlMYTzeq2KraMaSirAUpHhWM7LGwtIbnd-xhU2vnPSQP7pPdvZYzSXQn7VYqO2A/exec";
 
 // Rota para criar preferência do Mercado Pago
@@ -69,5 +69,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
+
 
 
